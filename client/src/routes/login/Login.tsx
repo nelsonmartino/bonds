2d766d456782dad53d7b4a2f3252c8ca6683c85c
nelsonmartino <nelsonmartino@gmail.com>
2024-09-05 11:45:51 -0300

    token storage

--- client/src/routes/login/Login.tsx
+++ client/src/routes/login/Login.tsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import { User } from '../../types'
 import { validateEmail } from '../../utils/validate'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 function Login() {
+  const navigate = useNavigate()
+
   const [form, setForm] = useState<User>({
     email: '',
     password: '',
   })
 
-  const [error, setError] = useState<string>('invisible')
+  const [hideError, setHideError] = useState<string>('invisible')
 
   const changeHandler = (
     e:
@@ -21,11 +25,23 @@ function Login() {
   }
 
   const mailHandler = (e: React.FocusEvent<HTMLInputElement>) => {
-    setError(validateEmail(e.target.value))
+    setHideError(validateEmail(e.target.value))
   }
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    if (hideError && form.email && form.password) {
+      await axios
+        .post('http://localhost:3001/login', form)
+        .then((res) =>
+          localStorage.setItem(
+            'loggedUser',
+            JSON.stringify({ email: form.email, token: res.data })
+          )
+        )
+        .catch((e) => console.error(e))
+      navigate('/bonds')
+    }
   }
 
   return (
@@ -58,12 +74,13 @@ function Login() {
                   type="email"
                   autoComplete="email"
                   required
+                  onChange={changeHandler}
                   onBlur={mailHandler}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
               <div
-                className={`relative block w-full p-3 mb-4 leading-5 ${error} text-white bg-red-500 rounded-lg opacity-100 font-regular`}
+                className={`relative block w-full p-3 mb-4 leading-5 ${hideError} text-white bg-red-500 rounded-lg opacity-100 font-regular`}
               >
                 Invalid email
               </div>
